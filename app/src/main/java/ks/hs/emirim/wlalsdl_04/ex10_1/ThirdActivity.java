735971b4994024ef83aa4741bff85f265a8b30cb
jimin <w2037@e-mirim.hs.kr>
2021-07-19 10:31:04 +0900

    Change activity_main And Add ThirdActivity, activity_third.xml

---
+++ app/src/main/java/ks/hs/emirim/wlalsdl_04/ex10_1/ThirdActivity.java
@@ -0,0 +1,16 @@
+package ks.hs.emirim.wlalsdl_04.ex10_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class ThirdActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_third);
+    }
+    Button
+}
